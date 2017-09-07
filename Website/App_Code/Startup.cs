3e578820ea33f5467039cb60cdbe9b6208b2bb5d
ccabrera3 <ccabrera3@studentmail.nait.ca>
2017-09-07 13:49:10 -0600

    Create Sample1 solution project

---
+++ Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sample1.Startup))]
+namespace Sample1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
